79fbf622ad5f94ec56d23e6d14c8ba3b5bfb135f
AdamKnube <adamk@5csecurity.com>
2014-06-11 18:14:01 -0400

    new file:   install-prereqs.sh 	modified:   install-tools.sh
    Broke up the install-tools script into 2 seperate parts.
    Also the prerequisites should now work with arch linux.

---
+++ install-prereqs.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+#
+
+
+use strict;
+
+my @packages;
+my $dist = `uname -a`;
+
+if (($dist =~ /ubuntu/) || ($dist =~ /debian/)) { 
+  print "Building for Ubuntu Linux.\n";
+  # Get libs for debian based distros.
+  # These are NOT ALL required but I consider them all to be 
+  # essential if you do any kind of serious building. 
+  #
+  print "Acquiring updated package lists...\n";
+  system('sudo apt-get update');
+  print "Acquiring packages...\n";
+  $packages[0] = 'sudo apt-get install';
+  $packages[1] = 'python-dev';
+  $packages[2] = 'subversion';
+  $packages[3] = 'build-essential';
+  $packages[4] = 'devscripts';
+  $packages[5] = 'automake';
+  $packages[6] = 'autoconf';
+  $packages[7] = 'bison';
+  $packages[8] = 'gawk';
+  $packages[9] = 'python2-dev';
+  $packages[10] = 'python-scapy';
+  $packages[11] = 'python2-scapy';
+  $packages[12] = 'libsqlite0-dev';
+  $packages[13] = 'libsqlite3-dev';
+  $packages[14] = 'libpcap-dev';
+  $packages[15] = 'libssl-dev'; 
+  $packages[16] = 'libnl-dev';
+  $packages[17] = 'python-distutils-extra'
+  $packages[18] = 'python2-distutils-extra';
+  $packages[19] = 'cmake';
+}
+elsif ($dist =~ /ARCH/) {
+  print "Building for Arch Linux.\n";
+  # Get libs for arch based distros.
+  # These are NOT ALL required but I consider them all to be 
+  # essential if you do any kind of serious building. 
+  #
+  print "Acquiring updated package lists...\n";
+  system('sudo pacman -Sy');
+  print "Acquiring packages...\n";
+  $packages[0] = 'sudo pacman -S';
+  $packages[1] = 'base-devel';
+  $packages[2] = 'subversion';
+  $packages[3] = 'cmake';
+  $packages[4] = 'autoconf';
+  $packages[5] = 'automake';
+  $packages[6] = 'bison';
+  $packages[7] = 'gawk';
+  $packages[8] = 'python-scapy';
+  $packages[9] = 'python2-scapy';
+  $packages[10] = 'sqlite';
+  $packages[11] = 'libpcap';
+  $packages[12] = 'openssl';
+  $packages[13] = 'libnl';
+  $packages[14] = 'python-distutils-extra'
+  $packages[15] = 'python2-distutils-extra';
+}
+else { die "Error: Don't know how to build for \"$dist\"\n"; }
+
+my $getpacks = join(' ', @packages);
+system($getpacks);
+exit(0);
--- install-tools.sh
+++ install-tools.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 #
 
-# Get libs for debian based distros.
-# These are NOT ALL required but I consider them all to be 
-# essential if you do any kind of serious building. 
-#
-sudo apt-get update
-sudo apt-get install \
-python-dev \
-subversion \
-build-essential \
-devscripts \
-automake \
-autoconf \
-bison \
-gawk \
-python-dev \
-python-scapy \
-libsqlite0-dev \
-libsqlite3-dev \
-libpcap-dev \
-libssl-dev \ 
-libnl-dev
-
-# Now we acquire/build aircrack-ng
+# Acquire/build aircrack-ng
 svn co http://svn.aircrack-ng.org/trunk/ aircrack
 cd aircrack
 make unstable=true sqlite=true
