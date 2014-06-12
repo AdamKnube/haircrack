1c94482a72be8c9e7f7297030e829bb36946bbe8
AdamKnube <adamk@5csecurity.com>
2014-06-12 17:10:20 -0400

    Changes to be committed: 	new file:   arch.pre 	modified:   setup.sh 	new file:   ubuntu.pre
    Modularity is the key.
    Still most effort being put into the installer.

--- setup.sh
+++ setup.sh
@@ -1,67 +1,98 @@
 #!/usr/bin/perl
 #
+# Still got a long way to go...
+# Need to label dependancies for what tool they belong to
+# and then only install those depends. For now it only 
+# pays attention to reaver. If reaver is installed then
+# we assume libpcap needs removing and recompiling.
+#
 
-
+# Includes
 use strict;
+use Getopt::Long;
 
-my @packages;
+# Globals
+my $reav = 0;
+my $airc = 0;
+my $pyrt = 0;
+my $iall = 1;
+my $debug = 0;
+my $pfil = '';
+my $pman = '';
+my $pupc = '';
+my $pinc = '';
+
+# Initialize
+GetOptions(
+  'debug+' => \$debug,
+  'aircrack+' => \$airc,
+  'reaver+' => $reav,
+  'pyrit+' => pyrt,
+);
+if ($reav || $airc || $pyrt) { $iall = 0; }
 my $dist = `uname -a`;
-
-if (($dist =~ /ubuntu/) || ($dist =~ /debian/)) { 
-  print "Building for Ubuntu Linux.\n";
-  # Get libs for debian based distros.
-  print "Acquiring updated package lists...\n";
-  system('sudo apt-get update');
-  print "Acquiring packages...\n";
-  $packages[0] = 'sudo apt-get install';
-  $packages[1] = 'python-dev';
-  $packages[2] = 'subversion';
-  $packages[3] = 'build-essential';
-  $packages[4] = 'devscripts';
-  $packages[5] = 'automake';
-  $packages[6] = 'autoconf';
-  $packages[7] = 'bison';
-  $packages[8] = 'gawk';
-  $packages[9] = 'python2-dev';
-  $packages[10] = 'python-scapy';
-  $packages[11] = 'libsqlite0-dev';
-  $packages[12] = 'libsqlite3-dev';
-  $packages[13] = 'cmake';
-  $packages[14] = 'libpcap-dev';
-  $packages[15] = 'libssl-dev'; 
-  $packages[16] = 'libnl-dev';
-  $packages[17] = 'python-distutils-extra';
-  $packages[18] = 'python2-distutils-extra';
+if ($dist =~ /ubuntu/) {
+  dprint('Installing for Ubuntu Linux.', 1);
+  $pfil = 'ubuntu.pre';
+  $pman = 'apt-get';
+  $pupc = 'update';
+  $pinc = 'install';
+  $prem = 'purge libpcap libpcap-dev';
 }
 elsif ($dist =~ /ARCH/) {
-  print "Building for Arch Linux.\n";
-  # Get libs for arch based distros.
-  print "Acquiring updated package lists...\n";
-  system('sudo pacman -Sy');
-  print "Acquiring packages...\n";
-  $packages[0] = 'sudo pacman -S';
-  $packages[1] = 'base-devel';
-  $packages[2] = 'subversion';
-  $packages[3] = 'cmake';
-  $packages[4] = 'autoconf';
-  $packages[5] = 'automake';
-  $packages[6] = 'bison';
-  $packages[7] = 'gawk';
-  $packages[8] = 'scapy';
-  $packages[9] = 'python2-distutils-extra';
-  $packages[10] = 'sqlite';
-  $packages[11] = 'python-distutils-extra';
-  $packages[12] = 'openssl';
-  $packages[13] = 'libnl';
-  $packages[14] = 'wget';  
-  my $getpacks = join(' ', @packages);
-  system($getpacks);
-  if (`pacman -Ss libpcap` =~ /installed/) { 
-    print "Removing libpcap...\n";
-    system('sudo pacman -Rdd libpcap');
-  }
-  print "Installing libpcap1.4.0...\n";
-  system('./arch-pcap.sh');
+  dprint('Installing for Arch Linux.', 1);
+  $pfil = 'arch.pre';
+  $pman = 'pacman';
+  $pupc = '-Sy';
+  $pinc = '-S';
+  $prem = '-Rdd libpcap';
 }
 else { die "Error: Don't know how to build for \"$dist\"\n"; }
+
+# Update the system package lists
+dprint('Acquiring updated package lists..', 1);
+system("sudo $pman $pupc");
+
+# Get the required packages
+my @paklist;
+dprint('Reading package list...', 1);
+open(PAKLIST, "<", $pfil) || die "Error: Can't find $pfil!\n";
+my @paks = <PAKLIST>;
+close(PAKLIST);
+foreach my $pak (@paks) {
+  $pak =~ s/^\s+//;
+  $pak =~ s/\s+$//; 
+  $pak =~ s/\n//;
+  if (($pak !~ /^#/) && ($pak ne '')) { push(@paklist, $pak); }
+}
+dprint('Acquiring packages...', 1);
+system("sudo $pman $pinc " . join(' ',@paklist));
+
+# Remove and downgrade libpcap if required
+if ($reav || $iall) { 
+  dprint('Removing libpcap...', 1);
+  system("sudo $pman $prem");
+  dprint('Installing libpcap1.4.0...', 1); 
+  system('pcap.sh');
+}
+
+# Install the requested/required tools
+if ($airc || $iall) { system ('aircrack.sh'); }
+if ($reav || $iall) { system ('reaver.sh'); }
+if ($pyrt || $iall) { system ('pyrit.sh'); }
+
+# Death by natural causes
 exit(0);
+
+# Functions/Subs
+sub dprint() {
+  my $data = shift;
+  my $force = shift;
+  if ($debug || $force) { print "$data\n"; }
+  if ($debug > 1) {
+	my trampstamp = `date +[%m/%d/%y-%H:%M:%S]`;
+	open(LOGG, ">>", "$0.log") || die "Can't open() the logfile\n";
+	print LOGG $trampstamp . " $data\n";
+	close(LOGG);
+  }
+}
